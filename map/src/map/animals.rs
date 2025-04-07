0a7e5ca10fd768bd0f2b5dde46fd3dfbae221c13
SchwarzRene <20191028@atn.ac.at>
2025-04-07 18:00:00 +0200

    file setup

--- map/src/map/animals.rs
+++ map/src/map/animals.rs
@@ -1,34 +1,40 @@
 pub mod rabit;
 
-mod animal {
-    use super::rabit;
 
-    enum Gender {
-        Male,
-        Female
+#[derive(PartialEq)]
+pub enum Gender {
+    Male,
+    Female
+}
+
+#[derive(PartialEq)]
+pub enum Typ {
+    Rabit
+}
+
+pub struct AnimalData{
+    typ : Typ,
+    gender : Typ,
+    speed : f32,
+    hunger : u8,
+    lust : u8,
+    thirst : u8,
+    position : Vec<usize>
+}
+
+pub struct Animal{
+    typ : Typ,
+    gender : Gender
+}
+
+impl Animal {
+    fn setup( typ : Typ ){
+        if typ == Typ::Rabit{
+            let animal_typ = rabit::Rabit::new();
+        }
     }
 
-    #[derive(PartialEq)]
-    enum Typ {
-        Rabit
-    }
-
-    struct Properties{
-        speed : f32,
-        hunger : f32,
-        lust : f32,
-        thirst : f32,
-        position : Vec<usize>,
-        gender : Gender,
-        typ : Typ,
-        search_radius : f32
-    }
-    
-    pub fn setup( typ : Typ ){
-        let new_anmial = rabit::setup();
-    }
-
-    pub fn update( ){
+    fn update(){
 
     }
 }
--- map/src/map/animals/rabit.rs
+++ map/src/map/animals/rabit.rs
@@ -1,9 +1,9 @@
 pub struct Rabit{
-
+    speed : f32
 }
 
-impl Rabit {
-    pub fn setup( &self ) -> Self{
-        Rabit{}
+impl Rabit{
+    pub fn new(){
+        
     }
 }
